446f85ec75a487c34d99e6cbca6b55a2eb56ebef
gapGit01 <gamaara2020@gmail.com>
2021-09-13 22:49:20 +0530

    Remove bad code and updete litle bit.

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+
+
 Itoms = {"apple":20, "milk":30, "banana":20, "pepar":10, "coffe":20, "speaker":100,
         "mouse_pad":40, "book":20, "cake":30, "keybord":90, "a":10, "s":20, "d":50}
 
@@ -8,27 +10,23 @@ count = 0
 n = 0
 stop = "ok"
 stop2 = "that's it"
-stop3 = "That's it"
 
 
 while True:
 
-    if customer_inputs != stop:
+    if customer_inputs != stop and stop2:
         customer_inputs = str(input("Enetr your food itom: ")).lower()
+        quantity = int(input(f"How many {customer_inputs} you wont: ")) 
 
-        if customer_inputs != stop and stop2 and stop3:
-            pass
-            quantity = int(input(f"How many {customer_inputs} you wont: "))
-
-        if customer_inputs == (stop2 or stop3):
+        if customer_inputs == (stop2):
             print(f"\n you'er total Payment is R.S {count}\n Thank you come again!!!!\n")
             break
 
 
-
-    if customer_inputs in Itoms:
-        count = count + (quantity * (Itoms[customer_inputs])) 
-        n = n + 1
+        """Main logic"""
+        if customer_inputs in Itoms:
+            count = count + (quantity * (Itoms[customer_inputs])) 
+            n = n + 1
 
     elif customer_inputs == stop:
         print(f"Total Payments R.S {count}\nThank you come again!!!!")
@@ -38,4 +36,3 @@ while True:
     else:
         print(f"{customer_inputs} is out of stocke")
         print(f"Total Payments R.S {count}")
-
